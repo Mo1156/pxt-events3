606aea5e74a10007213db759311de271b633f59d
Mo1156 <58610705+Mo1156@users.noreply.github.com>
2020-01-07 13:14:26 -0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-
+let random_number_610 = 0
+let random_number_15 = Math.randomRange(1, 5)
+game.splash("this is a number 1-5" + ";" + random_number_15, "")
+random_number_15 = Math.randomRange(6, 10)
+game.splash("this is a number 6-10" + ";" + random_number_610, "")
+let random_number_1120 = Math.randomRange(11, 20)
+game.splash("this is a number 11-20" + ";" + random_number_1120, "")
+let random_number_2125 = Math.randomRange(20, 25)
+game.splash("this is a number 21-25" + ";" + random_number_2125, "")
